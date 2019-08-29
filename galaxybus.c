547b123ac01837e5c4b7dbd87859e79fb0e7e795
Adrian Kennard <a@k.gg>
2019-08-29 11:13:39 +0100

    Finally working

--- galaxybus.c
+++ galaxybus.c
@@ -6,97 +6,346 @@ static const char TAG[] = "galaxybus";
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
-#include <driver/uart.h>
+#include <driver/timer.h>
 #include <driver/gpio.h>
 
-#define	GROUP_TOUT 1
 #define MASTER 0x11
 
 struct galaxybus_s
 {
-   int8_t uart;
-   uint8_t slave;
-   uint8_t address;             // Us
    EventGroupHandle_t group;
-   uart_dev_t u;
-   uint8_t rx[64];
-   uint8_t rxlen;
-   uint8_t rxseq;
-   uint8_t rxbusy:1;
+   int8_t timer;                // Which timer
+   int8_t tx;                   // Tx pin
+   int8_t rx;                   // Rx pin (can be same as tx)
+   int8_t de;                   // DE pin
+   int8_t re;                   // RE pin (optional, can be same as DE)
+   int8_t clk;                  // Debug pin (optional)
+   uint8_t address;             // Us
+   uint32_t txpre;              // Pre tx drive(high) (bits)
+   uint32_t txpost;             // Post tx drive(high) (bits)
+   uint32_t gap;                // Gap after which we assume end of message (bits)
+   uint32_t txgap;              // We are sending pre / post drive
+   uint8_t txlen;               // The length of tx buf
+   volatile uint8_t txpos;      // Position in tx buf we are sending(checked by app level)
+   uint8_t txdata[GALAXYBUSMAX];        // The tx message
+   int8_t rxerr;                //  The current in progress message rx error
+   int8_t rxerrorreport;        // The rxerror of the last stored message
+   uint8_t rxpos;               // Where we are in rx buf
+   uint8_t rxlen;               // Length of last received mesage in rxbuf
+   uint8_t rxdue;               // The expected message sequence
+   uint8_t rxsum;               // The current checksum
+   uint8_t rxgap;               // The remaining end of message timeout
+   volatile uint8_t rxseq;      // The last received message sequence
+   uint8_t rxdata[GALAXYBUSMAX];        // The Rx data
+   uint8_t subbit;              // Sub bit count
+   uint8_t bit;                 // Bit count
+   uint8_t shift;               // Byte
+   uint8_t slave:1;             // We are slave
+   uint8_t started:1;           // Int handler started
+   uint8_t txrx:1;              // Mode, true for rx , false for tx //Tx
+   uint8_t txdue:1;             // We are due to send a message
+   uint8_t txhold:1;            // We are in app Tx call and need to hold of sending as copying to buffer
+   uint8_t rxignore:1;          // This message is not for us so being ignored
+   uint8_t tick:1;              // clk tick
 };
 
-static int icount = 0;;
+#define TIMER_DIVIDER         16        //  Hardware timer clock divider
+#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
+
+#define	GROUP_RX_OK	1       // Rx is not busy
+#define	GROUP_TX_OK	2       // Tx is not busy
+#define	GROUP_RX_READY	4       // New Rx message ready
+
+// Low level direct GPIO controls
+static inline void
+gpio_in (int8_t r)
+{
+   if (r >= 32)
+      GPIO_REG_WRITE (GPIO_ENABLE1_W1TC_REG, 1 << (r - 32));
+   else if (r >= 0)
+      GPIO_REG_WRITE (GPIO_ENABLE_W1TC_REG, 1 << r);
+}
+
+static inline void
+gpio_out (int8_t r)
+{
+   if (r >= 32)
+      GPIO_REG_WRITE (GPIO_ENABLE1_W1TS_REG, 1 << (r - 32));
+   else if (r >= 0)
+      GPIO_REG_WRITE (GPIO_ENABLE_W1TS_REG, 1 << r);
+}
+
+static inline void
+gpio_set (int8_t r)
+{
+   if (r >= 32)
+      GPIO_REG_WRITE (GPIO_OUT1_W1TS_REG, 1 << (r - 32));
+   else if (r >= 0)
+      GPIO_REG_WRITE (GPIO_OUT_W1TS_REG, 1 << r);
+}
+
+static inline void
+gpio_clr (int8_t r)
+{
+   if (r >= 32)
+      GPIO_REG_WRITE (GPIO_OUT1_W1TC_REG, 1 << (r - 32));
+   else if (r >= 0)
+      GPIO_REG_WRITE (GPIO_OUT_W1TC_REG, 1 << r);
+}
+
+static inline uint32_t
+gpio_get (int8_t r)
+{
+   if (r >= 32)
+      return (GPIO_REG_READ (GPIO_IN1_REG) >> (r - 32)) & 1;
+   else if (r >= 0)
+      return (GPIO_REG_READ (GPIO_IN_REG) >> r) & 1;
+   else
+      return 0;
+}
+
+static inline void
+rs485_mode_rx (galaxybus_t * g)
+{                               // Switch to rx mode
+   if (g->tx == g->rx)
+      gpio_in (g->rx);          // Input
+   gpio_clr (g->de);
+   g->txrx = 1;                 // Rx mode
+}
+
+static inline void
+rs485_mode_tx (galaxybus_t * g)
+{                               // Switch to tx mode
+   gpio_set (g->de);
+   if (g->tx == g->rx)
+      gpio_out (g->rx);
+   g->txgap = g->txpre;
+   g->txdue = 0;
+   g->txrx = 0;                 // Tx mode
+}
 
 void IRAM_ATTR
-rx_int (void *gp)
+timer_isr (void *gp)
 {
    galaxybus_t *g = gp;
-#if 0
-   uint16_t status = g->u.int_st.val;   // read UART interrupt Status
-   uint16_t i = 0,
-      rx_fifo_len = g->u.status.rxfifo_cnt;     // read number of bytes in UART buffer
-   uint8_t rxbuf[100];
-   while (rx_fifo_len)
+   timer_group_intr_clr_in_isr (TIMER_GROUP_0, g->timer);
+   timer_group_enable_alarm_in_isr (TIMER_GROUP_0, g->timer);
+   if (g->clk >= 0)
    {
-      rxbuf[i] = g->u.fifo.rw_byte;   // read all bytes
-      rx_fifo_len--;
+      g->tick = 1 - g->tick;
+      if (g->tick)
+         gpio_set (g->clk);
+      else
+         gpio_clr (g->clk);
    }
-#endif
-   uart_clear_intr_status (g->uart, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR);
-   //xEventGroupSetBits (g->group, GROUP_TOUT);
-   icount++;
+   if (g->txrx)
+   {                            // Rx
+      uint8_t v = gpio_get (g->rx);
+      if (!v && !g->bit)
+      {                         // Idle, and low, so this is start of start bit
+         g->subbit = 1;         // Centre of start bit (+/- 1/6 of a bit)
+         g->bit = 10;
+      }
+      if (g->subbit--)
+         return;
+      g->subbit = 2;            // Three sub bits per bit
+      if (!g->bit)
+      {                         // Idle
+         if (g->rxgap)
+            g->rxgap--;
+         else
+         {                      // End of rx
+            g->rxignore = 0;
+            if (g->rxpos)
+            {
+               // Message received
+               if (g->rxsum != g->rxdata[g->rxpos - 1])
+                  g->rxerr = GALAXYBUSCHECKSUM;
+               g->rxlen = g->rxpos;
+               g->rxerrorreport = g->rxerr;
+               g->rxseq++;
+               if (g->slave)
+                  g->txdue = 1; // Send reply as we are slave
+               g->rxpos = 0;    // ready for next message
+               xEventGroupSetBits (g->group, GROUP_RX_READY);
+               xEventGroupSetBits (g->group, GROUP_RX_OK);
+            }
+            if (g->txdue)
+               rs485_mode_tx (g);       // Can start tx
+         }
+         return;
+      }
+      g->bit--;
+      if (g->bit == 9)
+      {                         // Start bit
+         if (v)
+         {                      // Missing start bit
+            g->rxerr = GALAXYBUSSTARTBIT;
+            g->bit = 0;         // Back to idle
+         }
+         return;
+      }
+      if (g->bit)
+      {                         // Shift in
+         g->shift >>= 1;
+         if (v)
+            g->shift |= 0x80;
+         return;
+      }
+      if (!g->rxpos)
+         g->rxerr = 0;          // Clear errors, new message
+      // Stop bit
+      if (!v)
+         g->rxerr = GALAXYBUSSTOPBIT;   // Missing stop bit
+      g->rxgap = g->gap;        // Look for end of message
+      // Checksum logic
+      if (!g->rxpos)
+         g->rxsum = 0xAA;
+      else
+      {
+         uint8_t l = g->rxdata[g->rxpos - 1];
+         if ((int) g->rxsum + l > 0xFF)
+            g->rxsum++;         // 1's comp
+         g->rxsum += l;
+      }
+      if (!g->rxpos && g->shift != g->address && g->address != 0xFF && g->shift != 0xFF)
+         g->rxignore = 1;       // Not addressed to us, ignore
+      if (g->rxignore)
+         return;                // Not for us
+      if (!g->rxpos)
+         xEventGroupClearBits (g->group, GROUP_RX_OK);
+      // End of byte
+      if (g->rxpos >= GALAXYBUSMAX)
+         g->rxerr = GALAXYBUSTOOBIG;
+      else
+         g->rxdata[g->rxpos++] = g->shift;
+      return;
+   }
+   // Tx
+   if (g->subbit--)
+      return;
+   g->subbit = 2;               // Three sub bits per bit
+   uint8_t t = 1;
+   if (g->txgap)
+   {
+      t = 1;
+      g->txgap--;
+      if (!g->txgap)
+      {                         // End of gap
+         if (g->txpos)
+         {                      // End of message
+            g->txpos = 0;
+            xEventGroupSetBits (g->group, GROUP_TX_OK);
+            rs485_mode_rx (g);  // Switch back to rx
+            return;
+         }
+         // Start of message
+         if (g->txhold)
+            g->txgap++;         // Wait, app is writing new message
+         else
+         {                      // Start sending
+            xEventGroupClearBits (g->group, GROUP_RX_OK);
+            g->bit = 9;
+            g->shift = g->txdata[g->txpos++];
+         }
+      }
+   } else if (g->bit)
+   {
+      if (g->bit == 9)
+         t = 0;                 // Start bit
+      else
+      {                         // Data bit
+         t = (g->shift & 1);
+         g->shift >>= 1;
+      }
+      g->bit--;
+   } else
+   {                            // Stop bit and prep next byte
+      if (g->txpos < g->txlen)
+      {
+         g->shift = g->txdata[g->txpos++];
+         g->bit = 9;
+      } else
+         g->txgap = g->txpost;  // End of message
+   }
+   if (t)
+      gpio_set (g->tx);
+   else
+      gpio_clr (g->tx);
 }
 
 // Set up
 galaxybus_t *
-galaxybus_init (int8_t uart, int8_t tx, int8_t rx, int8_t de, int8_t re, uint8_t slave)
+galaxybus_init (int8_t timer, int8_t tx, int8_t rx, int8_t de, int8_t re, int8_t clk, uint8_t slave)
 {
-   if (uart < 0 || uart > 2 || tx < 0 || rx < 0 || de < 0 || tx == rx || tx == de || rx == de || tx == re || rx == re)
+   if (timer < 0 || tx < 0 || rx < 0 || de < 0 || tx == de || rx == de || tx == re || rx == re)
       return NULL;
-   if (!GPIO_IS_VALID_OUTPUT_GPIO (tx) || !GPIO_IS_VALID_GPIO (rx) || !GPIO_IS_VALID_OUTPUT_GPIO (de)
-       || (re >= 0 && !GPIO_IS_VALID_OUTPUT_GPIO (re)))
+   if (!GPIO_IS_VALID_OUTPUT_GPIO (tx)  //
+       || !GPIO_IS_VALID_GPIO (rx)      //
+       || !GPIO_IS_VALID_OUTPUT_GPIO (de)       //
+       || (re >= 0 && !GPIO_IS_VALID_OUTPUT_GPIO (re))  //
+       || (clk >= 0 && !GPIO_IS_VALID_OUTPUT_GPIO (clk))        //
+      )
       return NULL;
-   if (re >= 0 && re != de)
-   {                            // Enable
-      gpio_set_level (re, 0);
-      gpio_set_direction (re, GPIO_MODE_OUTPUT);
-   }
    galaxybus_t *g = malloc (sizeof (*g));
    if (!g)
       return g;
    memset (g, 0, sizeof (*g));
-   if (uart == 0)
-      g->u = UART0;
-   else if (uart == 1)
-      g->u = UART1;
-   else if (uart == 2)
-      g->u = UART2;
-   else return NULL; // WTF
-   g->slave=slave;
-   g->uart = uart;
+   g->txpre = 10;               // defaults
+   g->txpost = 10;
+   g->gap = 10;
+   g->de = de;
+   g->re = re;
+   g->tx = tx;
+   g->rx = rx;
+   g->clk = clk;
+   g->timer = timer;
+   g->slave = (slave ? 1 : 0);
+   g->address = (slave ? : MASTER);
    g->group = xEventGroupCreate ();
-   // Init UART
-   uart_config_t uart_config = {
-      .baud_rate = 9600,
-      .data_bits = UART_DATA_8_BITS,
-      .parity = UART_PARITY_DISABLE,
-      .stop_bits = UART_STOP_BITS_1,
-      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-   };
-   esp_err_t err;
-   if ((err = uart_param_config (uart, &uart_config)) || (err = uart_set_pin (uart, tx, rx, de, -1))
-       || (err = uart_driver_install (uart, 129, 129, 0, NULL, 0)) || (err = uart_set_mode (uart, UART_MODE_RS485_HALF_DUPLEX))
-       || (err = uart_set_rx_timeout (uart, 1)))
-   {
-      ESP_LOGE (TAG, "UART fail %s", esp_err_to_name (err));
-      free (g);
-      return NULL;
-   }
-   //uart_isr_free (uart);
-   uart_isr_register (uart, rx_int, g, ESP_INTR_FLAG_IRAM, NULL);
-   uart_enable_rx_intr (uart);
-   ESP_LOGD (TAG, "PN532 UART %d Tx %d Rx %d", uart, tx, rx);
    return g;
+}
+
+void
+galaxybus_set_timing (galaxybus_t * g, uint32_t pre, uint32_t post, uint32_t gap)
+{
+   if (pre)
+      g->txpre = pre;
+   if (post)
+      g->txpost = post;
+   if (gap)
+      g->gap = gap;
+}
+
+void
+galaxybus_start (galaxybus_t * g)
+{
+   g->started = 1;
+   // GPIO (does nothing for -ve port IDs)
+   gpio_clr (g->de);
+   gpio_out (g->de);
+   gpio_clr (g->re);
+   gpio_out (g->re);
+   gpio_out (g->clk);
+   if (g->tx != g->rx)
+      gpio_out (g->tx);
+   // Set up timer
+   timer_config_t config;
+   config.divider = TIMER_DIVIDER;
+   config.counter_dir = TIMER_COUNT_UP;
+   config.counter_en = TIMER_PAUSE;
+   config.alarm_en = TIMER_ALARM_EN;
+   config.intr_type = TIMER_INTR_LEVEL;
+   config.auto_reload = 1;
+   timer_init (TIMER_GROUP_0, g->timer, &config);
+   timer_set_counter_value (TIMER_GROUP_0, g->timer, 0x00000000ULL);
+   timer_set_alarm_value (TIMER_GROUP_0, g->timer, TIMER_SCALE / 9600 / 3);
+   timer_enable_intr (TIMER_GROUP_0, g->timer);
+   timer_isr_register (TIMER_GROUP_0, g->timer, timer_isr, g, ESP_INTR_FLAG_IRAM, NULL);
+   timer_start (TIMER_GROUP_0, g->timer);
+   xEventGroupSetBits (g->group, GROUP_TX_OK);
+   xEventGroupSetBits (g->group, GROUP_RX_OK);
+   rs485_mode_rx (g);
 }
 
 void *
@@ -104,60 +353,72 @@ galaxybus_end (galaxybus_t * g)
 {
    if (g)
    {
-      uart_disable_rx_intr (g->uart);
-      uart_driver_delete (g->uart);
+      if (g->started)
+      {
+         timer_disable_intr (TIMER_GROUP_0, g->timer);
+      }
       free (g);
    }
    return NULL;
 }
 
 // Low level messaging
-void
-galaxybus_tx (galaxybus_t * g, int len, uint8_t * buf)
-{
-   uint32_t c = 0xAA,
-      p;
-   for (p = 0; p < len; p++)
-      c += buf[p];
-   while (c > 0xFF)
-      c = (c >> 8) + (c & 0xFF);
-   uint8_t csum = c;
-   uart_write_bytes (g->uart, (char *) buf, len);
-   uart_write_bytes (g->uart, (char *) &csum, 1);
-   uart_wait_tx_done (g->uart, (len + 1) * 10 * 1000000 / 9600 / portTICK_PERIOD_MS);
-}
-
 int
-galaxybus_rx (galaxybus_t * g, int max, uint8_t * buf)
+galaxybus_tx (galaxybus_t * g, int len, uint8_t * data)
 {
-   if (!xEventGroupWaitBits (g->group, GROUP_TOUT, pdTRUE, pdTRUE, 100 / portTICK_PERIOD_MS))
+   if (len >= GALAXYBUSMAX)
+      return GALAXYBUSTOOBIG;
+   g->txhold = 1;               // Stop sending starting whilst we are loading
+   if (!xEventGroupWaitBits (g->group, GROUP_TX_OK, pdFALSE, pdTRUE, 100 / portTICK_PERIOD_MS))
    {
-      ESP_LOGI (TAG, "Rx Timeout (icount=%d)", icount);
-#if 0                           // DoH
-      uart_flush (g->uart);
-      return -1;
-#endif
+      g->txhold = 0;
+      return GALAXYBUSBUSY;
    }
-   size_t waiting;
-   uart_get_buffered_data_len (g->uart, &waiting);      // TODO interrupt kick
-   ESP_LOGI (TAG, "Waiting %d", waiting);
-   if (waiting > max)
+   uint8_t c = 0xAA;
+   int p;
+   for (p = 0; p < len; p++)
    {
-      uart_flush (g->uart);
-      return -1;
+      g->txdata[p] = data[p];
+      if ((int) c + data[p] > 0xFF)
+         c++;                   // 1 's comp
+      c += data[p];
    }
-   int len = uart_read_bytes (g->uart, buf, waiting, 0);
-   // TODO address
-   // TODO checksum
+   g->txdata[p++] = c;          // Checksum
+   g->txlen = p;
+   if (!g->slave)
+      g->txdue = 1;             // Send now (if slave we send when polled)
+   g->txhold = 0;               // Allow sending
    return len;
 }
 
 int
-galaxybus_poll (galaxybus_t * g, uint8_t address, int max, uint8_t * buf)
+galaxybus_ready (galaxybus_t * g)
 {
-   uint8_t tx[2];
-   tx[0] = address;
-   tx[1] = 0x06;                // SIMple poll
-   galaxybus_tx (g, 2, tx);
-   return galaxybus_rx (g, max, buf);
+   if (g->rxdue == g->rxseq)
+      return 0;                 // Nothing ready
+   return 1;
+}
+
+int
+galaxybus_rx (galaxybus_t * g, int max, uint8_t * data)
+{
+   if (!xEventGroupWaitBits (g->group, GROUP_RX_OK, pdFALSE, pdTRUE, 10 / portTICK_PERIOD_MS))
+      return 0;
+   if (g->rxdue == g->rxseq)
+      return 0;                 // Nothing ready
+   g->rxdue++;
+   if (g->rxdue != g->rxseq)
+      return GALAXYBUSMISSED;   // Missed one
+   if (!g->rxlen)
+      return 0;                 // Uh?
+   if (g->rxerrorreport)
+      return g->rxerrorreport;  // Bad rx
+   if (g->rxlen > max)
+      return GALAXYBUSTOOBIG;   // No space
+   int p;
+   for (p = 0; p < g->rxlen - 1; p++)
+      data[p] = g->rxdata[p];
+   if (g->rxpos || g->rxdue != g->rxseq)
+      return GALAXYBUSMISSED;   // Missed one whilst reading data !
+   return p;
 }
--- include/galaxybus.h
+++ include/galaxybus.h
@@ -11,19 +11,24 @@
 #include <unistd.h>
 #include <malloc.h>
 
+#define GALAXYBUSMAX        64
+#define GALAXYBUSMISSED     -1
+#define GALAXYBUSTOOBIG     -2
+#define GALAXYBUSSTARTBIT   -3
+#define GALAXYBUSSTOPBIT    -4
+#define GALAXYBUSCHECKSUM   -5
+#define GALAXYBUSBUSY	   -6
+
 typedef struct galaxybus_s galaxybus_t;
 
 // Set up
-galaxybus_t *galaxybus_init (int8_t uart, int8_t tx, int8_t rx, int8_t de, int8_t re, uint8_t slave);
+galaxybus_t *galaxybus_init (int8_t timer, int8_t tx, int8_t rx, int8_t de, int8_t re, int8_t clk, uint8_t slave);
+void galaxybus_set_timing (galaxybus_t * g, uint32_t pre, uint32_t post, uint32_t gap);
+void galaxybus_start (galaxybus_t *);
 void *galaxybus_end (galaxybus_t *);
 
-// Low level messaging
-void galaxybus_tx (galaxybus_t *, int len, uint8_t * buf);
+int galaxybus_tx (galaxybus_t *, int len, uint8_t * buf);
+int galaxybus_ready (galaxybus_t *);
 int galaxybus_rx (galaxybus_t *, int max, uint8_t * buf);
-int galaxybus_poll (galaxybus_t *, uint8_t address, int max, uint8_t * buf);
-
-
-// Higher level task as slave
-// TODO
 
 #endif
